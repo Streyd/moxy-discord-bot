bddf9a25e079b897d301c0a4f0552443cf226a2a
Streyd <58256271+Streyd@users.noreply.github.com>
2020-05-11 21:21:32 +0700

    Update db_session.py

--- data/db_session.py
+++ data/db_session.py
@@ -20,15 +20,15 @@ def global_init(db_file):
 
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     
-    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
-    print(f"Подключение к базе данных по адресу {conn_str}")
+    #conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    #print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False)
-    __factory = orm.sessionmaker(bind=engine)
+    #engine = sa.create_engine(conn_str, echo=False)
+    #__factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
 
-    SqlAlchemyBase.metadata.create_all(engine)
+    #SqlAlchemyBase.metadata.create_all(engine)
 
 
 def create_session() -> Session:
